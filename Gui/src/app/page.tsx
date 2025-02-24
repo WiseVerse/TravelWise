e4a8fe527fea708f5d177ef6c5c773d70a1c8a7a
MoG <moritz.graschopf@gmail.com>
2025-02-24 14:10:00 +0100

    Test

--- Gui/src/app/page.tsx
+++ Gui/src/app/page.tsx
@@ -1,7 +1,11 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
     return (
-        <div>
-
+        <div className="h-screen flex justify-center items-center">
+            <Button>
+                Goon
+            </Button>
         </div>
     );
 }
