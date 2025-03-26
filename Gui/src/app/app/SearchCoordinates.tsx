b6748f9fab45f13a9367bda848d959e8b05a785e
Moritz Graschopf <moritz.graschopf@gmail.com>
2025-03-26 14:14:18 +0100

    added SearchForm and Info Card

---
+++ Gui/src/app/app/SearchCoordinates.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import React from "react";
+import {Form, FormControl, FormDescription, FormField, FormLabel, FormItem, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+
+const coordinateRegex = /^(\d{1,2}\.\d{5,})°[NS] (\d{1,3}\.\d{5,})°[EW]$/;
+
+const formSchema = z.object({
+    coordinates: z.string().regex(coordinateRegex, "Ungültiges Koordinatenformat. Mindestens 5 Dezimalstellen. Beispiel: 48.40999°N 15.60384°E"),
+})
+
+export default function SearchCoordinates() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            coordinates: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="coordinates"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Koordinaten</FormLabel>
+                            <FormControl>
+                                <Input placeholder="48.40999°N 15.60384°E" {...field} />
+                            </FormControl>
+                            <FormDescription className="sr-only">
+                                Coordinates
+                            </FormDescription>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
--- Gui/src/app/app/page.tsx
+++ Gui/src/app/app/page.tsx
@@ -1,11 +1,57 @@
-import {Button} from "@/components/ui/button";
+import SearchAddress from "@/app/app/SearchAddress";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import React from "react";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import SearchCoordinates from "@/app/app/SearchCoordinates";
+import {ScrollArea} from "@/components/ui/scroll-area";
 
 export default function Home() {
     return (
-        <div className="h-screen flex justify-center items-center">
-            <Button>
-                Goon
-            </Button>
+        <div className="h-full w-[20vw] m-2 grid grid-cols-1 grid-rows-[auto_1fr] gap-4">
+            <Tabs defaultValue="addresses">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="addresses">Adresse</TabsTrigger>
+                    <TabsTrigger value="coordinates">Koordinaten</TabsTrigger>
+                </TabsList>
+                <TabsContent value="addresses">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Suche</CardTitle>
+                            <CardDescription>Suchen Sie nach Adressen</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <SearchAddress/>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="coordinates">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Suche</CardTitle>
+                            <CardDescription>Suchen Sie mit Koordinaten</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <SearchCoordinates/>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+            <Card className="flex flex-col h-full max-h-screen">
+                <CardHeader>
+                    <CardTitle>Info</CardTitle>
+                    <CardDescription>Info zum Ort Musterstraße 1</CardDescription>
+                </CardHeader>
+                <CardContent className="flex-1 overflow-hidden">
+                    <ScrollArea className="h-full max-h-screen overflow-auto">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie libero lacus, at feugiat
+                        diam porta pulvinar. Vestibulum mollis, orci eu convallis lobortis, justo magna tempus purus, in
+                        euismod turpis diam eu odio. Morbi sit amet pellentesque lectus. Praesent euismod odio ut diam
+                        ullamcorper, non commodo orci varius. Phasellus volutpat lacinia tortor vitae ullamcorper. Morbi
+                        consequat augue tincidunt, lacinia arcu eu, consectetur ipsum. Nam maximus leo sed magna posuere
+                        rhoncus. Pellentesque dignissim tellus vestibulum elit luctus tincidunt.
+                    </ScrollArea>
+                </CardContent>
+            </Card>
         </div>
     );
 }
