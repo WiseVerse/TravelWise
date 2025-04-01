d595d972793e745021154eb364ebdf378d146fd2
Moritz Graschopf <moritz.graschopf@gmail.com>
2025-04-01 14:36:19 +0200

    hallo

--- Gui/src/app/app/search/page.tsx
+++ Gui/src/app/app/search/page.tsx
@@ -5,9 +5,11 @@ import SearchAddress from "@/components/search/address";
 import SearchCoordinates from "@/components/search/coordinates";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {ScrollArea} from "@/components/ui/scroll-area";
 import SiteHeader from "@/components/site-header";
 import MapComponent, {MapComponentRef} from "@/components/map/map";
+import {Button} from "@/components/ui/button";
+import {SquareArrowOutUpRight} from "lucide-react";
+import Link from "next/link";
 
 export default function SearchPage() {
 
@@ -62,92 +64,25 @@ export default function SearchPage() {
                         </TabsContent>
                     </Tabs>
                     
-                    <Card className="h-full">
+                    <Card>
                         <CardHeader>
-                            <CardTitle>Info</CardTitle>
-                            <CardDescription>Djowiajodiwjoa</CardDescription>
+                            <CardTitle>Chat</CardTitle>
+                            <CardDescription>Weiter zum Chat</CardDescription>
                         </CardHeader>
                         <CardContent>
-                            <p>djwaiojodia</p>
+                            <Button size="icon" variant="outline" asChild>
+                                <Link href="">
+                                    <SquareArrowOutUpRight/>
+                                    
+                                </Link>
+                                
+                            </Button>
                         </CardContent>
                     </Card>
                 </div>
                 
                 <MapComponent ref={mapRef}/>
             </div>
-
-
-            {/*<div className="relative w-screen" style={{ height: "calc(100vh - 4rem)" }}>*/}
-            {/*    /!* Karte im Hintergrund *!/*/}
-            {/*    <div className="absolute inset-0 z-0">*/}
-            {/*        <MapComponent ref={mapRef} />*/}
-            {/*    </div>*/}
-
-            {/*    /!* Overlay für die UI, oben links positioniert *!/*/}
-            {/*    <div className="absolute top-0 left-0 z-10 p-4">*/}
-            {/*        /!* Container für Tabs/UI – z.B. feste Breite  w-96 *!/*/}
-            {/*        <div className="w-96 space-y-4 bg-transparent p-4 rounded">*/}
-            {/*            /!* TABS *!/*/}
-            {/*            <Tabs defaultValue="addresses" className="w-full">*/}
-            {/*                /!* TabsList inline-flex, damit die Tabs nur so breit wie ihr Inhalt sind *!/*/}
-            {/*                <TabsList className="inline-flex space-x-2">*/}
-            {/*                    <TabsTrigger value="addresses">Adresse</TabsTrigger>*/}
-            {/*                    <TabsTrigger value="coordinates">Koordinaten</TabsTrigger>*/}
-            {/*                </TabsList>*/}
-
-            {/*                /!* Inhalt Tab "Adresse" *!/*/}
-            {/*                <TabsContent value="addresses" className="pt-4">*/}
-            {/*                    <Card>*/}
-            {/*                        <CardHeader>*/}
-            {/*                            <CardTitle>Suche</CardTitle>*/}
-            {/*                            <CardDescription>Suchen Sie nach Adressen</CardDescription>*/}
-            {/*                        </CardHeader>*/}
-            {/*                        <CardContent>*/}
-            {/*                            <SearchAddress onSearch={handleAddressSearch} />*/}
-            {/*                        </CardContent>*/}
-            {/*                    </Card>*/}
-            {/*                </TabsContent>*/}
-
-            {/*                /!* Inhalt Tab "Koordinaten" *!/*/}
-            {/*                <TabsContent value="coordinates" className="pt-4">*/}
-            {/*                    <Card>*/}
-            {/*                        <CardHeader>*/}
-            {/*                            <CardTitle>Suche</CardTitle>*/}
-            {/*                            <CardDescription>Suchen Sie mit Koordinaten</CardDescription>*/}
-            {/*                        </CardHeader>*/}
-            {/*                        <CardContent>*/}
-            {/*                            <SearchCoordinates />*/}
-            {/*                        </CardContent>*/}
-            {/*                    </Card>*/}
-            {/*                </TabsContent>*/}
-            {/*            </Tabs>*/}
-
-            {/*            /!* INFO-CARD *!/*/}
-            {/*            <Card className="flex flex-col h-[40vh]">*/}
-            {/*                <CardHeader>*/}
-            {/*                    <CardTitle>Info</CardTitle>*/}
-            {/*                    <CardDescription>Info zum Ort Musterstraße 1</CardDescription>*/}
-            {/*                </CardHeader>*/}
-            {/*                <CardContent className="flex-1 overflow-hidden min-h-0">*/}
-            {/*                    <ScrollArea className="h-full overflow-y-auto">*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam*/}
-            {/*                        molestie libero lacus, at feugiat diam porta pulvinar...*/}
-            {/*                    </ScrollArea>*/}
-            {/*                </CardContent>*/}
-            {/*            </Card>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </>
     );
 }
