4189c5bf720a41ccec6b6f337846b25185ab7a76
Moritz Graschopf <moritz.graschopf@gmail.com>
2025-04-03 15:31:28 +0200

    Mobile Version so ca.

--- Gui/src/components/site-header.tsx
+++ Gui/src/components/site-header.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import {SidebarTrigger} from "@/components/ui/sidebar";
 import {Separator} from "@/components/ui/separator";
-import React from "react";
+import React, {useEffect} from "react";
 import {
     Breadcrumb,
     BreadcrumbItem, BreadcrumbLink,
@@ -8,15 +10,45 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
+import {useIsMobile} from "@/hooks/use-mobile";
+import {Button} from "@/components/ui/button";
+import SiteSearch from "@/components/site-search";
+import {Menu} from "lucide-react";
+import {chat} from "@/lib/types";
+import {fetchChats} from "@/lib/fetch";
 
 export default function SiteHeader({title, variant="default"}: { title: string, variant?: "default" | "chats" }) {
+    const isMobile = useIsMobile()
+    const [open, setOpen] = React.useState(false);
+    const [chats, setChats] = React.useState<chat[]>([]);
+    useEffect(() => {
+        setOpen(false);
+    }, [isMobile]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchChats().then(data => setChats(data));
+        }
+        
+        fetchData().then()
+    }, [])
+    
     switch (variant) {
         case "default":
             return (
                 <header
                     className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
                     <div className="flex w-full items-center gap-1 px-4">
-                        <SidebarTrigger className="-ml-1"/>
+                        {isMobile ? (
+                            <>
+                                <Button variant="ghost" size="icon" onClick={() => setOpen(true)}>
+                                    <Menu/>
+                                </Button>
+                                <SiteSearch open={open} setOpen={setOpen} chats={chats}/>
+                            </>
+                        ) : (
+                            <SidebarTrigger className="-ml-1"/>
+                        )}
                         <Separator
                             orientation="vertical"
                             className="mx-2 data-[orientation=vertical]:h-4"
